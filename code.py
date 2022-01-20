29943fe4ed3f60004af5a939cdf5c78dd7f7933a
Josefin <98112479+josefinstr@users.noreply.github.com>
2022-01-20 17:47:41 +0100

    Create code.py

---
+++ code.py
@@ -0,0 +1,10 @@
+def mysum(sequence):
+    """
+    Returns the sum of the elements in sequence
+    """
+    # YOUR CODE HERE
+    sumOfElems = 0
+    for element in sequence:
+        sumOfElems += element
+    return sumOfElems
+    raise NotImplementedError()
